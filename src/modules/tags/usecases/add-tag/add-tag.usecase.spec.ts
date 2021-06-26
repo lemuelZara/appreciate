842dc8e6aa194955d6ab331f4daba140dd344df2
Lemuel <lemuel.czara@gmail.com>
2021-06-26 14:47:36 -0300

    test: ensure AddTagUseCase calls findByName with correct params

--- src/modules/tags/usecases/add-tag/add-tag.usecase.spec.ts
+++ src/modules/tags/usecases/add-tag/add-tag.usecase.spec.ts
@@ -2,12 +2,20 @@ import { AddTagUseCase } from './add-tag.usecase';
 
 import { TagRepositoryProtocols } from '~modules/tags/infra/protocols';
 import { TagRepository } from '~modules/tags/infra/repositories';
+import { Tag } from '~modules/tags/entities';
 import { AddTagDTO } from '~modules/tags/dtos';
 
 import { BadRequestException } from '~shared/errors/http-errors';
 
 const makeFakeTagData = (): AddTagDTO => ({
   name: 'valid_name'
+});
+
+const makeFakeTag = (): Tag => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  createdAt: new Date(),
+  updatedAt: new Date()
 });
 
 describe('AddTagUseCase', () => {
@@ -34,5 +42,15 @@ describe('AddTagUseCase', () => {
     await expect(usecase.execute(tagData)).rejects.toEqual(
       new BadRequestException('Name not provided!')
     );
+  });
+
+  test('should be called findByName with correct params', async () => {
+    const tagData = makeFakeTagData();
+
+    jest.spyOn(repository, 'findByName').mockResolvedValue(null);
+
+    await usecase.execute(tagData);
+
+    expect(repository.findByName).toBeCalledWith(tagData.name);
   });
 });
