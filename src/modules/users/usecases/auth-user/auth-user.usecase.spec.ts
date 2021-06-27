6c31b6e6b9973cc222b517a15774f36ae6881f48
Lemuel <lemuel.czara@gmail.com>
2021-06-27 13:29:04 -0300

    test: ensure AuthUseUseCase calls findByEmail with correct params

---
+++ src/modules/users/usecases/auth-user/auth-user.usecase.spec.ts
@@ -0,0 +1,62 @@
+import { AuthUserUseCase } from './auth-user.usecase';
+
+import { User } from '~modules/users/entities';
+import { UserRepositoryProtocols } from '~modules/users/infra/protocols';
+import { UserRepository } from '~modules/users/infra/repositories';
+import { AuthUserDTO } from '~modules/users/dtos';
+
+import { CryptoProtocols } from '~shared/container/providers/crypto/protocols';
+import { BCryptProvider } from '~shared/container/providers/crypto/bcrypt';
+import { JsonWebTokenProvider } from '~shared/container/providers/jwt/impl/json-web-token.provider';
+import { JWTProtocols } from '~shared/container/providers/jwt/protocols';
+import { BadRequestException } from '~shared/errors/http-errors';
+
+const createdAt = new Date();
+const updatedAt = new Date();
+
+const makeFakeAuthData = (): AuthUserDTO => ({
+  email: 'any_email',
+  password: 'any_password'
+});
+
+const makeFakeUser = (): User => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email',
+  admin: true,
+  password: 'valid_password',
+  createdAt,
+  updatedAt
+});
+
+describe('AuthUserUseCase', () => {
+  let usecase: AuthUserUseCase;
+  let repository: UserRepositoryProtocols;
+  let cryptoProvider: CryptoProtocols;
+  let jwtProvider: JWTProtocols;
+
+  beforeEach(() => {
+    repository = new UserRepository();
+    cryptoProvider = new BCryptProvider(12);
+    jwtProvider = new JsonWebTokenProvider();
+    usecase = new AuthUserUseCase(repository, cryptoProvider, jwtProvider);
+
+    repository.findByEmail = jest.fn();
+
+    cryptoProvider.compare = jest.fn();
+    jwtProvider.generateToken = jest.fn();
+  });
+
+  test('should be definied', () => {
+    expect(usecase).toBeDefined();
+  });
+
+  test('should be called findByEmail with correct params', async () => {
+    jest.spyOn(cryptoProvider, 'compare').mockResolvedValueOnce(true);
+    jest.spyOn(repository, 'findByEmail').mockResolvedValueOnce(makeFakeUser());
+
+    await usecase.execute(makeFakeAuthData());
+
+    expect(repository.findByEmail).toHaveBeenCalledWith('any_email');
+  });
+});
