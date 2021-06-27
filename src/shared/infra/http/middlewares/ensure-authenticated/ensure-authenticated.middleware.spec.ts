5dfc0603d761825116e16344dc831dcd0c20d937
Lemuel <lemuel.czara@gmail.com>
2021-06-27 18:29:29 -0300

    test: ensure EnsureAuthenticatedMiddleware calls decodeToken with correct params

--- src/shared/infra/http/middlewares/ensure-authenticated/ensure-authenticated.middleware.spec.ts
+++ src/shared/infra/http/middlewares/ensure-authenticated/ensure-authenticated.middleware.spec.ts
@@ -6,14 +6,18 @@ import { JWTProtocols } from '~shared/container/providers/jwt/protocols';
 import { JsonWebTokenProvider } from '~shared/container/providers/jwt/impl/json-web-token.provider';
 import { UnauthorizedException } from '~shared/errors/http-errors';
 
+import auth from '~config/auth';
+
 describe('EnsureAuthenticatedMiddleware', () => {
   let middleware: EnsureAuthenticatedMiddleware;
   let provider: JWTProtocols;
-  const httpRequest = {
-    headers: {
-      authorization: `Bearer any_token`
-    }
-  } as Request;
+
+  const makeFakeHttpRequest = () =>
+    ({
+      headers: {
+        authorization: 'Bearer any_token'
+      }
+    } as Request);
 
   const httpResponse = {
     status: jest.fn(() => httpResponse),
@@ -25,6 +29,9 @@ describe('EnsureAuthenticatedMiddleware', () => {
   beforeEach(() => {
     provider = new JsonWebTokenProvider();
     middleware = new EnsureAuthenticatedMiddleware(provider);
+
+    provider.decodeToken = jest.fn();
+    auth.publicKey = 'any_secret';
   });
 
   test('should be definied', () => {
@@ -32,12 +39,29 @@ describe('EnsureAuthenticatedMiddleware', () => {
   });
 
   test('should be throw if authHeader is empty', async () => {
-    httpRequest.headers.authorization = '';
+    const httpRequest = makeFakeHttpRequest();
+
+    httpRequest.headers = {};
 
     await expect(
       middleware.handle(httpRequest, httpResponse, next)
     ).rejects.toEqual(
       new UnauthorizedException('Authentication token is missing!')
+    );
+  });
+
+  test('should be called decodeToken with correct params', async () => {
+    const httpRequest = makeFakeHttpRequest();
+
+    jest.spyOn(provider, 'decodeToken').mockReturnValueOnce({
+      sub: 'any_userId'
+    });
+
+    await middleware.handle(httpRequest, httpResponse, next);
+
+    expect(provider.decodeToken).toHaveBeenCalledWith(
+      'any_token',
+      'any_secret'
     );
   });
 });
